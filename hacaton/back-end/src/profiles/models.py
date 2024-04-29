8b0caf882f7b3a7cf0ec0255789bb9081bbfcb9f
Сергей <po4ka2001@mail.ru>
2024-04-29 22:29:35 +0300

    создана модель пользователя

--- hacaton/back-end/src/profiles/models.py
+++ hacaton/back-end/src/profiles/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class University(models.Model):
+    name = models.CharField(max_length=100)
+
+class Speciality(models.Model):
+    name = models.CharField(max_length=100)
+
+class TypeUser(models.Model):
+    name = models.CharField(max_length=100)
+
+class Profile(models.Model):
+    firstname = models.CharField(max_length=100)
+    secondname = models.CharField(max_length=100)
+    therename = models.CharField(max_length=100)
+    type = models.ForeignKey(TypeUser, on_delete=models.CASCADE)
+
+class Student(Profile):
+    university = models.ForeignKey(University, on_delete=models.CASCADE)
+    speciality = models.ForeignKey(Speciality, on_delete=models.CASCADE)
+
+class Business(Profile):
+    name_company = models.CharField(max_length=100)
+    inn_company = models.IntegerField()
