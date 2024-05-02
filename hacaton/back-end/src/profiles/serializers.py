5bbc9c24b7fec9ea2a6e335d86517c531cb08e7d
Сергей <po4ka2001@mail.ru>
2024-05-02 10:23:49 +0300

    добавление в сериалайзер модель студента и бизнеса

--- hacaton/back-end/src/profiles/serializers.py
+++ hacaton/back-end/src/profiles/serializers.py
@@ -5,3 +5,13 @@ class ProfilesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profiles
         field = ('id', 'user', 'type', 'firstname', 'secondname', 'therename')
+
+class StudentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profiles
+        field = ('id', 'profile', 'university', 'speciality')
+
+class BusinesesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profiles
+        field = ('id', 'profile', 'name_company', 'inn_company', 'possition')
