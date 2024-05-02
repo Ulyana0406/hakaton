9e221ee60de5b4d57f99226d0c7b6863dfbc54a2
Сергей <po4ka2001@mail.ru>
2024-05-02 20:16:24 +0300

    Переделана логика coworking и аудиторий

--- hacaton/back-end/src/reservation/models.py
+++ hacaton/back-end/src/reservation/models.py
@@ -1,23 +1,13 @@
 from django.db import models
 from event.models import Event
-
-class Corpuses(models.Model):
-    class Meta:
-        verbose_name = 'Корпус'
-        verbose_name_plural = 'Корпуса'
-    name = models.CharField(max_length=100, verbose_name='Корпус')
-
-class Auditories(models.Model):
-    class Meta:
-        verbose_name = 'Аудиторию'
-        verbose_name_plural = 'Аудитории'
-    name = models.CharField(max_length=100, verbose_name='Аудитория')
-    corpus = models.ForeignKey(Corpuses, on_delete=models.CASCADE, verbose_name='Корпус')
+from coworking.models import Auditories
+from profiles.models import Profiles
 
 class Reservations(models.Model):
     class Meta:
         verbose_name = 'Бронирование'
         verbose_name_plural = 'Забронировать'
+    user = models.ForeignKey(Profiles, on_delete=models.CASCADE, verbose_name='Логин')
     location = models.ForeignKey(Auditories, on_delete=models.CASCADE, verbose_name='Аудитория')
     event = models.ForeignKey(Event, on_delete=models.CASCADE, verbose_name='Мероприятие')
     datatime = models.DateTimeField(verbose_name='Время')
