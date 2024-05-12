7771e820e66c9569c3286f0e89b63fd02b628304
Sergey <po4ka2001@mail.ru>
2024-05-13 00:10:24 +0300

    Изменения шапки

--- hacaton/front-end/src/components/Header/Header.tsx
+++ hacaton/front-end/src/components/Header/Header.tsx
@@ -1,31 +1,32 @@
 import styles from "./Header.module.scss";
 import { useNavigate } from "react-router-dom";
+import Cookies from 'js-cookie';
 function Header() {
+  const authToken: string | undefined = Cookies.get('sessionid');
   const navigate = useNavigate();
   const redirectMain = () => {
     navigate("/");
   };
   return (
-    <>
-      <div className={styles.header}>
-        <img onClick={redirectMain} src="Лого.png" alt="" />
+    <div className={styles.header}>
+      <img onClick={redirectMain} src="Лого.png" alt="Лого" />
+      {authToken ? (
         <div className={styles.headerhrefs}>
-          <a className={styles.hrefs} href="events">
-            Мероприятия
-          </a>
-          <a className={styles.hrefs} href="">
-            Coworking
-          </a>
-          <a className={styles.hrefs} href="">
-            Бронирование
-          </a>
+          <a className={styles.hrefs} href="events">Мероприятия</a>
+          <a className={styles.hrefs} href="coworking">Coworking</a>
+          <a className={styles.hrefs} href="booking">Бронирование</a>
+          <div className={styles.headerButtons}>
+              <button className={styles.enterButton}>Вход</button>
+              <button className={styles.registerButton}>Регистрация</button>
+            </div>
         </div>
+      ) : (
         <div className={styles.headerButtons}>
-          <button className={styles.enterButton}>Вход</button>
+          <button className={styles.enterButton} >Вход</button>
           <button className={styles.registerButton}>Регистрация</button>
         </div>
-      </div>
-    </>
+      )}
+    </div>
   );
 }
 
